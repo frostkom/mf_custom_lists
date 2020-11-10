5c601248df95448db776ff1af0afcad0a1deb4c6
frostkom <martin.fors@frostkom.se>
2020-11-10 12:25:18 +0100

    Change post type settings to not show up in sitemap

--- include/classes.php
+++ include/classes.php
@@ -43,7 +43,7 @@ class mf_custom_list
 
 		$args = array(
 			'labels' => $labels,
-			'public' => true,
+			'public' => false, // Previously true but changed to hide in sitemap.xml
 			'show_ui' => true,
 			'show_in_menu' => false,
 			'show_in_nav_menus' => false,
