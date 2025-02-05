03b353a4b9bdf54eea4e8982116ba6f4b7bcfca7
frostkom <martin.fors@frostkom.se>
2025-02-05 10:22:13 +0100

    Make script load in footer

--- include/classes.php
+++ include/classes.php
@@ -124,7 +124,7 @@ class mf_custom_list
 		$plugin_include_url = plugin_dir_url(__FILE__);
 		$plugin_version = get_plugin_version(__FILE__);
 
-		wp_register_script('script_custom_lists_block_wp', $plugin_include_url."block/script_wp.js", array('wp-blocks', 'wp-element', 'wp-components', 'wp-editor', 'wp-block-editor'), $plugin_version);
+		wp_register_script('script_custom_lists_block_wp', $plugin_include_url."block/script_wp.js", array('wp-blocks', 'wp-element', 'wp-components', 'wp-editor', 'wp-block-editor'), $plugin_version, true);
 
 		$arr_data = array();
 		get_post_children(array('add_choose_here' => true, 'post_type' => $this->post_type), $arr_data);
